fec15bbb3d52db2cfbdc4e918abfbfed2ad2b085
LongQ <longquoc47krb@gmail.com>
2022-07-14 12:07:05 +0700

    Create Home

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import welcome from "../../assets/banner.jpg";
+
 function Home() {
   return (
     <div className='main'>
       <div className='main-container'>
         <img src={welcome} />
+
+        <div className='content-container'>
+          <p>Download Shop Coin USA App</p>
+          <span>Manage crypto assets at your fingertips</span>
+          <button className='download'></button>
+        </div>
       </div>
     </div>
   );
